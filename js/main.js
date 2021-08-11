f4c441f6255c353d72b729a44a6ba18f6b4d1493
usakocher <adammkocher@gmail.com>
2021-08-11 09:04:58 -0400

    Problem #3

--- js/main.js
+++ js/main.js
@@ -69,3 +69,33 @@ console.log(jennifer.printInfo())
 console.log(james.age)
 james.addAge(3)
 console.log(james.age)
+
+// =============Exercise #3 ============//
+/*
+
+    Create a Promised based function that will check a string to determine if it's length is greater than 10.
+    If the length is greater than ten console log "Big word". 
+    If the length of the string is less than 10 console log "Small Number"
+*/
+
+const wordLength = (s) =>{
+    return new Promise( (resolve, reject) => {
+        if(s.length > 10){
+            resolve(true)
+        }else{
+            reject(false)
+        }
+    } )
+};
+
+wordLength('Elevletters').then( (result) => {
+    console.log('Big word')
+} ).catch( (error) => {
+    console.log('Small Number')
+} )
+
+wordLength('Tenletters').then( (result) => {
+    console.log('Big word')
+} ).catch( (error) => {
+    console.log('Small Number')
+} )
